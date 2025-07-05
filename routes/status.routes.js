0277db8f1cf4e8e988938d803d26ab58e0d3c49c
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-07-05 10:14:48 -0500

    test: add MongoDB connection setup and teardown in tests; increase Jest timeout to avoid hanging

--- routes/status.routes.js
+++ routes/status.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createStatus } = require('../controllers/status.controller');
 
-router.post('adoption/status', createStatus);
+router.post('/adoption/status', createStatus);
 
 module.exports = router;
