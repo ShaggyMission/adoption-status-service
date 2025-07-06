3a607e78c47e421690249f392b28e2a968272161
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-07-05 19:43:58 -0500

    chore: update MongoDB connection to use statusPets_db with authSource

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const mongoURI = 'mongodb://admin:Lis12345@54.156.27.209:27017/statusPets_db?authSource=admin';
+
   try {
-    await mongoose.connect('mongodb://admin:Lis12345@54.156.27.209:27017/admin', {
-      dbName: 'adoption_db',
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected to adoption_db');
-  } catch (err) {
-    console.error('MongoDB connection failed:', err.message);
+    console.log('MongoDB connected to statusPets_db');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
